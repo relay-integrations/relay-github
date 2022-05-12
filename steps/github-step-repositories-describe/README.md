221ca1dc228e96901b9382ead14c8f09a1aca0ba
Ben Ford <ben.ford@puppetlabs.com>
2022-05-12 16:39:34 -0700

    Add a step to describe repos in a github org

---
+++ steps/github-step-repositories-describe/README.md
@@ -0,0 +1,6 @@
+# github-step-repositories-describe
+
+This step will describe the repositories belonging to an organization. It requires a
+token with the proper access, otherwise you'll very quickly hit GitHub API rate-limiting.
+
+See [spec.schema.json](spec.schema.json) for parameters.
---
+++ steps/github-step-repositories-describe/step.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from github import Github, UnknownObjectException
+from relay_sdk import Interface, Dynamic as D
+
+relay = Interface()
+
+try:
+  token = relay.get(D.token)
+  org = relay.get(D.org)
+except:
+  raise ValueError("Token and org are required parameters")
+
+scope = relay.get(D.scope)
+sort = relay.get(D.sort)
+direction = relay.get(D.direction)
+
+gh = Github(token)
+
+try:
+  repos = gh.get_organization(org).get_repos(type=scope, sort=sort, direction=direction)
+  repos = list(map(lambda x: x.raw_data, repos))
+except UnknownObjectException as err:
+  print(f'Unknown org {org}.')
+  raise
+
+relay.outputs.set("repositories", repos)
