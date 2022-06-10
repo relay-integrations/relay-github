8683e742d3f2d53d15f2ea44cc53fb0e7bec9bbc
Ben Ford <ben.ford@puppetlabs.com>
2022-06-10 16:30:49 -0700

    Add ability to filter for desired fields
    The returned list of repositories is often too large of a payload. By
    default, this just lists the repository names, but allows the user to
    specify a list of additional fields they'd like to return.

--- steps/repositories-describe/README.md
+++ steps/repositories-describe/README.md
@@ -2,3 +2,13 @@
 
 This step will describe the repositories belonging to an organization. It requires a
 token with the proper access, otherwise you'll very quickly hit GitHub API rate-limiting.
+
+## Usage
+
+Pass a token and the org name, and then either the name or some parameters for
+filtering and ordering the list. If you pass a name, it will return a complete
+object representing that repository. If you pass filter/order params, it will
+return a list of all matching repos.
+
+By default, this list is only the full name of each repository. If you'd like
+more fields returned, list them in the `fields` parameter.
--- steps/repositories-describe/step.py
+++ steps/repositories-describe/step.py
@@ -11,6 +11,11 @@ except:
   raise ValueError("Token and org are required parameters")
 
 try:
+  name = relay.get(D.name)
+except:
+  name = None
+
+try:
   scope = relay.get(D.scope)
 except:
   scope = "all"
@@ -25,13 +30,28 @@ try:
 except:
   direction = "asc"
 
+try:
+  fields = relay.get(D.fields)
+except:
+  fields = []
+
 gh = Github(token)
 
 try:
-  repos = gh.get_organization(org).get_repos(type=scope, sort=sort, direction=direction)
-  repos = list(map(lambda x: x.raw_data, repos))
-except UnknownObjectException as err:
-  print(f'Unknown org {org}.')
-  raise
+  if name:
+    repo = gh.get_organization(org).get_repo(name)
+    relay.outputs.set("repository", repo.raw_data)
+  else:
+    repos = gh.get_organization(org).get_repos(type=scope, sort=sort, direction=direction)
+    repos = list(map(lambda x: x.raw_data, repos))
 
-relay.outputs.set("repositories", repos)
+    # ensure that the full_name is part of the fields; doesn't matter if it's there twice
+    fields.insert(0, 'full_name')
+
+    # now filter to have only the fields requested so that we don't exceed payload size when saving output.
+    repos = list(map(lambda repo: { key: repo[key] for key in fields }, repos))
+
+    relay.outputs.set("repositories", repos)
+except UnknownObjectException as err:
+  print(f'Unknown organization or repository.')
+  raise
